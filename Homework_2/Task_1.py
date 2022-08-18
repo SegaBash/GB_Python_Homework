b872053600aabe4be61c80b3a456a166905eb020
Sergey <svbashkirov@gmail.com>
2022-08-18 23:59:41 +0300

    Task 1 done

--- Homework_2/Task_1.py
+++ Homework_2/Task_1.py
@@ -1,3 +1,17 @@
 # Напишите программу, которая принимает на вход число N и выдает список факториалов для чисел от 1 до N.
 # пусть N = 4 -> [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+def factorial(n):
+    fact = 1
+    for i in range(2, n+1):
+        fact *= i
+    return fact
+
+n = int(input('Введите число '))
+fac_list = [0] * n
+
+for i in range(n):
+    fac_list[i] = factorial(i + 1)
+print(fac_list)
+
+
