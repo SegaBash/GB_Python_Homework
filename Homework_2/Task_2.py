940c4d5bf8a05355048988c9c537f17354b3d407
Sergey <svbashkirov@gmail.com>
2022-08-19 00:26:06 +0300

    Task 2 in process

--- Homework_2/Task_2.py
+++ Homework_2/Task_2.py
@@ -1,1 +1,9 @@
 # Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z
+
+print(f'X\tY\tZ\t¬(X ∧ Y) ∨ Z')
+false_true = range(2)
+for x in false_true:
+    for y in false_true:
+        for z in false_true:
+            
+            print(x, y, z, int(not ((x and y) or z)),  sep='\t')
