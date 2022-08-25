ba475ab7716706f9aab35f840241772ff69ff16e
Sergey_B <svbashkirov@gmail.com>
2022-08-25 17:27:12 +0300

    Task 1 is done

--- Homework_3/Task_1.py
+++ Homework_3/Task_1.py
@@ -1,2 +1,19 @@
-#Создайте файл. Запишите в него N первыхэлементов последовательности Фибоначчи.
+# Создайте файл. Запишите в него N первыхэлементов последовательности Фибоначчи.
+#
+
+def Fibo(n):
+    fibo = [1] * n
+    prev_value = current_value = 1
+    for i in range(2, n):
+        current_value, prev_value = prev_value + current_value, current_value
+        fibo[i] = current_value
+    return fibo
+
+n = int(input("Введите количество чисел Фибоначчи "))
+if n > 100:
+    print('Сам(a) считай!')
+    exit
+data = open('Homework_3/out.txt', 'w')
+data.write(str(Fibo(n)))
+data.close
 
