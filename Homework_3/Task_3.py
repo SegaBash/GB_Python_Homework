a980239712886a37dd8477d81353fe9902756f4a
Sergey_B <svbashkirov@gmail.com>
2022-08-26 18:19:40 +0300

    intermediate commit

--- Homework_3/Task_3.py
+++ Homework_3/Task_3.py
@@ -2,12 +2,17 @@
 # Бот должен, как минимум, отвечать на фразы «привет», «как тебя зовут».
 # Если фраза ему неизвестна, он выводит соответствующую фразу.
 
-def answer(question):
-    pass
+def answer(question:str):
+    bot_database = {
+    'привет' : 'здравствуйте',
+    'как тебя зовут' : 'у девочки нет имени'
+    }
+    question_list = list(bot_database)
+    return bot_database[question.lower()]
 
 
 
 user_input = input('Ваша фраза: ')
 while (user_input.lower() != 'выход'):
-    print(user_input)
+    print(answer(user_input))
     user_input = input('Ваша фраза: ')
